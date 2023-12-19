d2c700051de650f7096754d983f79d924a4d7e8a
oap75 <omid.asadpoor36@gmail.com>
2023-12-19 14:43:21 +0200

    4th test

--- Formula/sunodo.rb
+++ Formula/sunodo.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Sunodo < Formula
   desc "Sunodo CLI"
   homepage "https://github.com/sunodo/sunodo"
-  url "https://registry.npmjs.org/@sunodo/cli/-/cli-0.10.2.tgz"
-  sha256 "0db4932d762f39bd9c3f7010bcd53595101casdsd"
+  url "https://registry.npmjs.org/@sunodo/cli/-/cli-0.10.1.tgz"
+  sha256 "0db4932d762f39bd9c3f7010bcd53595101cadsd"
   license "Apache-2.0"
 
   livecheck do
